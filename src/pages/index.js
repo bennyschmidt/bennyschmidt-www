e92191badd664fe923766df632510525a37f1816
exactchange <benny@exactchange.network>
2023-12-09 17:00:55 -0800

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,216 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useState, useEffect } from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+import {
+  Great_Vibes as GreatVibes,
+  Inter
+} from 'next/font/google';
+
+const PORTFOLIO = [
+  {
+    id: "portfolio-item-0",
+    alt: "Game browser",
+    src: "/img/portfolio/item-0.png",
+    href: "https://dribbble.com/shots/22531499-Game-browser",
+    year: 2023
+  },
+  {
+    id: "portfolio-item-1",
+    alt: "New message",
+    src: "/img/portfolio/item-1.png",
+    href: "https://dribbble.com/shots/3039672-New-message",
+    year: 2016
+  },
+  {
+    id: "portfolio-item-2",
+    alt: "Peach Player",
+    src: "/img/portfolio/item-2.png",
+    href: "https://dribbble.com/shots/3688183-Peach-Player",
+    year: 2017
+  },
+  {
+    id: "portfolio-item-3",
+    alt: "Capitol.pn (The Hunger Games)",
+    src: "/img/portfolio/item-3.png",
+    href: "https://www.behance.net/bennyschmidt",
+    year: 2011
+  },
+  {
+    id: "portfolio-item-4",
+    alt: "Peach Player (iOS)",
+    src: "/img/portfolio/item-4.png",
+    href: "https://dribbble.com/shots/3688183-Peach-Player",
+    year: 2017
+  },
+  {
+    id: "portfolio-item-5",
+    alt: "Get a ride",
+    src: "/img/portfolio/item-5.png",
+    href: "https://dribbble.com/shots/3019741-Get-a-ride",
+    year: 2016
+  },
+  {
+    id: "portfolio-item-6",
+    alt: "Dusk browser",
+    src: "/img/portfolio/item-6.png",
+    href: "https://github.com/bennyschmidt/medici",
+    year: 2023
+  },
+  {
+    id: "portfolio-item-7",
+    alt: "Wave",
+    src: "/img/portfolio/item-7.png",
+    href: "https://www.behance.net/gallery/180474337/Social-media-app",
+    year: 2023
+  },
+  {
+    id: "portfolio-item-8",
+    alt: "Wyre",
+    src: "/img/portfolio/item-8.png",
+    href: "https://dribbble.com/shots/2991649-Send-money",
+    year: 2016
+  },
+  {
+    id: "portfolio-item-9",
+    alt: "Playlist",
+    src: "/img/portfolio/item-9.png",
+    href: "https://dribbble.com/shots/3688183-Peach-Player",
+    year: 2017
+  },
+  {
+    id: "portfolio-item-10",
+    alt: "City line icons",
+    src: "/img/portfolio/item-10.png",
+    href: "https://www.behance.net/gallery/54747979/World-Cities-Line-Icons",
+    year: 2014
+  },
+  {
+    id: "portfolio-item-11",
+    alt: "Early light/skeuomorphic",
+    src: "/img/portfolio/item-11.png",
+    href: "https://dribbble.com/shots/1649274-New-Message-flow",
+    year: 2014
+  },
+  {
+    id: "portfolio-item-12",
+    alt: "Web design",
+    src: "/img/portfolio/item-12.png",
+    href: "https://dribbble.com/shots/4370618-Landing-page",
+    year: 2018
+  }
+];
+
+const inter = Inter({
+  subsets: ['latin']
+});
+
+const greatVibes = GreatVibes({
+  subsets: ['latin'],
+  weight: '400'
+});
+
+const SocialMedia = () => (
+  <h3 className="flex gap-1 items-center justify-center mb-4">
+    <a href="https://www.dribbble.com/bennyschmidt" target="_blank">
+      <img alt="Benny Schmidt on Dribbble" src="/img/dribbble.svg" className="rounded" width="24px" height="24px" />
+    </a>
+    <a href="https://www.github.com/bennyschmidt" target="_blank">
+      <img alt="Benny Schmidt on GitHub" src="/img/github.svg" className="rounded" width="24px" height="24px" />
+    </a>
+  </h3>
+);
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [portfolio, setPortfolio] = useState();
+  const [colCount, setColCount] = useState();
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+  useEffect(() => {
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+    /**
+     * The responsive override is necessary to
+     * for a simpler stacking grid effect (like Pinterest).
+     */
+
+    setColCount(
+      window.innerWidth < 900 ? 1 : 3
+    );
+
+    if (PORTFOLIO?.length) {
+
+      /**
+       * Chunk the portfolio response into subarrays
+       * of equal length, except the final array which
+       * may include an extra item.
+       */
+
+      setPortfolio(
+        new Array(colCount)
+        .fill()
+        .map((_, index) => {
+          const chunkSize = (PORTFOLIO.length / colCount) << 0;
+          const chunkStart = chunkSize * index;
+
+          const chunkEnd = index < (colCount - 1)
+            ? chunkStart + chunkSize
+            : Infinity;
+
+          return PORTFOLIO.slice(chunkStart, chunkEnd);
+        })
+      );
+    }
+
+    const onResize = () => (
+      setColCount(window.innerWidth < 900 ? 1 : 3)
+    );
+
+    /**
+     * Handle window resize
+     */
+
+    addEventListener('resize', onResize);
+
+    return () => removeEventListener('resize', onResize);
+  }, [colCount]);
+
+  return !portfolio ? <div /> : (
+    <div className="min-h-screen">
+      <header>
+        <h1 className={`text-center text-5xl text-slate-600 mt-3 lowercase ${greatVibes.className}`}>
+          Benny
+        </h1>
+      </header>
+      <main className={`flex flex-col items-center ${inter.className}`}>
+        <h2 className="text-center text-sm text-slate-400 mt-4 mb-2">
+          Designer + Developer
+        </h2>
+        <SocialMedia />
+        <section className="flex gap-5 items-start justify-center w-5/6">
+          {portfolio.map((list, index) => (
+            <ul key={index} className="flex flex-col justify-start gap-5 list-none w-full">
+              {list.map(({ id, alt, src, href, year }) => (
+                <li key={id} className="rounded-lg w-full bg-slate-100 p-2 item">
+                  <a href={href} target="_blank">
+                    <img id={id} alt={alt} src={src} className="bg-slate-800 rounded-lg" width="100%" height="100%" />
+                    <h3 className="text-sm text-slate-800 font-semibold flex flex-column items-center mt-2">
+                      {alt}
+                    </h3>
+                    <p className="text-sm text-slate-500 flex flex-column items-center">
+                      {year}
+                    </p>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          ))}
+        </section>
+        <footer className="py-4">
+          <SocialMedia />
+          <p className="text-sm text-slate-600">
+            <a href="mailto:hello@bennyschmidt.com" target="_blank">
+              hello@bennyschmidt.com
+            </a>
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        </footer>
+      </main>
+    </div>
+  );
 }
