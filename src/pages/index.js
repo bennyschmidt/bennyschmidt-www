1621eff93f3bde26300f8403a960de769376237b
exactchange <benny@exactchange.network>
2023-12-16 07:31:42 -0800

    show fewer shots

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,10 +33,10 @@ const PORTFOLIO = [
   },
   {
     id: "portfolio-item-3",
-    alt: "Capitol.pn (The Hunger Games)",
-    src: "/img/portfolio/item-3.png",
-    href: "https://www.behance.net/bennyschmidt",
-    year: 2011
+    alt: "Get a ride",
+    src: "/img/portfolio/item-5.png",
+    href: "https://dribbble.com/shots/3019741-Get-a-ride",
+    year: 2016
   },
   {
     id: "portfolio-item-4",
@@ -47,59 +47,38 @@ const PORTFOLIO = [
   },
   {
     id: "portfolio-item-5",
-    alt: "Get a ride",
-    src: "/img/portfolio/item-5.png",
-    href: "https://dribbble.com/shots/3019741-Get-a-ride",
-    year: 2016
+    alt: "Capitol.pn (The Hunger Games)",
+    src: "/img/portfolio/item-3.png",
+    href: "https://www.behance.net/bennyschmidt",
+    year: 2011
   },
   {
     id: "portfolio-item-6",
-    alt: "Dusk browser",
-    src: "/img/portfolio/item-6.png",
-    href: "https://github.com/bennyschmidt/medici",
-    year: 2023
-  },
-  {
-    id: "portfolio-item-7",
-    alt: "Wave",
-    src: "/img/portfolio/item-7.png",
-    href: "https://www.behance.net/gallery/180474337/Social-media-app",
-    year: 2023
-  },
-  {
-    id: "portfolio-item-8",
     alt: "Wyre",
     src: "/img/portfolio/item-8.png",
     href: "https://dribbble.com/shots/2991649-Send-money",
     year: 2016
   },
   {
-    id: "portfolio-item-9",
+    id: "portfolio-item-7",
     alt: "Playlist",
     src: "/img/portfolio/item-9.png",
     href: "https://dribbble.com/shots/3688183-Peach-Player",
     year: 2017
   },
   {
-    id: "portfolio-item-10",
+    id: "portfolio-item-8",
     alt: "City line icons",
     src: "/img/portfolio/item-10.png",
     href: "https://www.behance.net/gallery/54747979/World-Cities-Line-Icons",
     year: 2014
   },
   {
-    id: "portfolio-item-11",
+    id: "portfolio-item-9",
     alt: "Early light/skeuomorphic",
     src: "/img/portfolio/item-11.png",
     href: "https://dribbble.com/shots/1649274-New-Message-flow",
     year: 2014
-  },
-  {
-    id: "portfolio-item-12",
-    alt: "Web design",
-    src: "/img/portfolio/item-12.png",
-    href: "https://dribbble.com/shots/4370618-Landing-page",
-    year: 2018
   }
 ];
 
