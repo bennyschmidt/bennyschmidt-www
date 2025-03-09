62eb274039cbb889bd45a35a149052d3edd3b512
bennyschmidt <hello@bennyschmidt.com>
2025-03-08 21:29:57 -0800

    project

--- src/pages/index.js
+++ src/pages/index.js
@@ -174,7 +174,7 @@ export default function Home() {
   return !portfolio ? <div /> : (
     <div className="min-h-screen max-w-[1600px] mx-auto">
       <aside
-        className="flex justify-between items-center bg-[url('/img/blacksand.jpg')] bg-center bg-cover rounded-[1rem] p-4 w-5/6 mx-auto my-4"
+        className="flex justify-between items-center bg-[url('/img/meteora.png')] bg-center bg-cover rounded-[1rem] p-4 w-5/6 mx-auto my-4"
         style={{ boxShadow: `0 0 3rem ${slate[500]}` }}
       >
         <div className="flex flex-col align-start justify-center">
@@ -182,18 +182,18 @@ export default function Home() {
             className="text-white"
             style={{ textShadow: '0 1px 0 black' }}
           >
-            Latest Project: Blacksand
+            Latest Project: METEORA
           </strong>
           {!full && <p
-            className="text-white opacity-50"
+            className="text-white"
             style={{ textShadow: '0 1px 0 black' }}
           >
-            Imagination at scale.
+            A classic role-playing experience rich with dialog and storytelling.
           </p>}
         </div>
         <a
           className="block flex flex-1 min-w-[8rem] max-w-[11rem] gap-2 align-center justify-center bg-white text-[black] text-center text-sm my-1 rounded-[1rem] cursor-pointer p-2 lg:p-4 hover:bg-black hover:text-[white]"
-          href="https://blacksand.cloud"
+          href="https://playmeteora.vercel.app/"
           target="_blank"
         >
           <span
